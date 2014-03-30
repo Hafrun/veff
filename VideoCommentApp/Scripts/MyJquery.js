fd8e05070e60235a6d539343af620142a21670a3
hafrun <hafruns@gmail.com>
2014-03-30 21:25:10 +0000

    veff

--- VideoCommentApp/Scripts/MyJquery.js
+++ VideoCommentApp/Scripts/MyJquery.js
@@ -1,19 +1,29 @@
-﻿$.getJSON('/Home/getAllComments', { name: 'model' },
+﻿var tempArray = new Array();
+
+$.getJSON('/Home/getAllComments', { name: 'model' },
 function (list) {
     $(".comments").loadTemplate($("#template"), list);
 })
 
-$(function(){ 
+function loadComments() {
+    // Athugum hvort við viljum appenda eða ekki
+    var append = false;
+    if ($('#comment-button').is(':checked')) {
+        append = true;
+    }
+    // Gagna fylkið okkar (með öllum commentum)
+        tempArray = [loadComments()];
 
-    var comments = getAllComments();
-    $.ajax({
-        url: "MyJquery.js", 
-        type: "POST",
-        data: comments,     
-        cache: false,
-        success: function(comment) {
-            alert($(".comments").loadTemplate($("#template"), list));
+        // Þetta yfirskrifar allt elementið með öllum commentunum
+        $('#comments').loadTemplate($('#template'), array, { append: append });
+}
 
-        }
+$(function () {
+    // Sækjum öll comment þegar síðan loadar
+    loadComments();
+
+    $('#comment-button').click(function () {
+        // Sækjum öll comment þegar notandi smellir á takkann
+        loadComments();
     });
-})
+});
